1f783d4666e197b3ea72275c07657a022646d710
Ammar <ammarmasud@gmail.com>
2017-02-13 21:04:10 +0000

    Added error handling in several ways. Read comments for details on each.

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 var request = require('request');
 var fs = require('fs');
 var GITHUB_USER = "Ammarmasud";
-var GITHUB_TOKEN = process.env.GITHUB_TOKEN
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -18,15 +18,41 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 // downloades images then pipes them to appropriate folder
 function downloadImageByURL(url, filePath) {
-  request.get(url).pipe(fs.createWriteStream('./' + filePath));
+  request.get(url)
+    .on('error', function (err) { throw err; })
+    .pipe(fs.createWriteStream('./' + filePath));
 }
 
+
 // Check if the correct number of arguments were given
-if (process.argv.length !== 4) {
-  console.log("Wrong number of arguments pass. Two arguments required: <Github repo owner> and <Github repo name>");
+if (process.argv.length !== 4 && process.argv.length !== 5) {
+  console.log(`Wrong number of arguments. Following arguments required...
+    Argument 1: <Github repo owner>
+    Argument 2: <Github repo name>
+    Optional argument 3: <Github API token>`);
+
 } else {
+  // check if github api token was given
+  if (process.argv.length === 5) {
+    var GITHUB_TOKEN = process.argv[4];
+  } else if (process.env.GITHUB_TOKEN) {
+    var GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  } else {
+    console.log("Github API key not found. Insert into .env file or input as third command line argument.")
+  }
+
+  // Check if avatars directory exists, make it otherwise
+  if (!fs.existsSync('./avatars')){
+    fs.mkdirSync('./avatars');
+  }
+
   // Continues script if correct arguments passed
   getRepoContributors(process.argv[2], process.argv[3], function(err, response, body) {
+    if (err) {
+      console.log("Errors:", err);
+    }
+
+    // loop through jason to download avatar for each user
     for (var user of JSON.parse(body)) {
       downloadImageByURL(user['avatar_url'], `avatars/${user['login']}.jpg`);
     }
