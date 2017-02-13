016b40c4a5725bf7c99e87fe32fd2d7039944230
Ammar <ammarmasud@gmail.com>
2017-02-13 17:41:18 +0000

    generated requestURL in the getRepoContributers function from username and a github token from another ignored file

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,12 @@
 var request = require('request');
+var GITHUB_USER = "Ammarmasud";
+var GITHUB_TOKEN = require("./github_token");
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 
