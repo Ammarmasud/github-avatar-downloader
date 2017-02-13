31dbb2ae4b1dd5ae0790548ce60c1c1bf80593b8
Ammar <ammarmasud@gmail.com>
2017-02-13 20:21:23 +0000

    added comments

--- download_avatars.js
+++ download_avatars.js
@@ -5,26 +5,29 @@ var GITHUB_TOKEN = require("./github_token");
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// retreives JSON from github API of contributers
 function getRepoContributors(repoOwner, repoName, cb) {
-  var options = { url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-                  headers: {"User-Agent":"GitHub Avatar Downloader - Student Project"}
+  // Includes User-agent header because it is required by the API
+  var options = {
+    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {"User-Agent": "GitHub Avatar Downloader - Student Project"}
   }
   request(options, cb);
 }
 
-
+// downloades images then pipes them to appropriate folder
 function downloadImageByURL(url, filePath) {
-  request.get(url)
-            .pipe(fs.createWriteStream('./' + filePath));
+  request.get(url).pipe(fs.createWriteStream('./' + filePath));
 }
 
-
+// Check if the correct number of arguments were given
 if (process.argv.length !== 4) {
   console.log("Wrong number of arguments pass. Two arguments required: <Github repo owner> and <Github repo name>");
 } else {
+  // Continues script if correct arguments passed
   getRepoContributors(process.argv[2], process.argv[3], function(err, response, body) {
     for (var user of JSON.parse(body)) {
-      downloadImageByURL(user['avatar_url'], `avatars/${user['login']}.jpg`)
+      downloadImageByURL(user['avatar_url'], `avatars/${user['login']}.jpg`);
     }
   });
 }
